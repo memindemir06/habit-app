9ab941617a9251b72c5befefb207a0c8236e316d
Mehmet Demir <mehmet.demir@student.manchester.ac.uk>
2021-04-02 03:12:02 +0100

    Map header styling

--- Habit/frontend/src/components/MapPage.js
+++ Habit/frontend/src/components/MapPage.js
@@ -6,9 +6,11 @@ import {
   LoadScript,
   Marker,
   InfoWindow,
+  Circle,
 } from "@react-google-maps/api";
 import {
   Button,
+  Collapse,
   Dialog,
   DialogActions,
   DialogContent,
@@ -17,6 +19,7 @@ import {
   Input,
   MenuItem,
   FormControl,
+  Grid,
   Select,
   Typography,
   Tooltip,
@@ -29,11 +32,20 @@ import {
 import { Link } from "react-router-dom";
 import FilterListIcon from "@material-ui/icons/FilterList";
 import LoadingPage from "./LoadingPage";
+import MuiDialogTitle from "@material-ui/core/DialogTitle";
 import CloseIcon from "@material-ui/icons/Close";
+import FingerprintIcon from "@material-ui/icons/Fingerprint";
+import GpsFixedIcon from "@material-ui/icons/GpsFixed";
+import ExpandLess from "@material-ui/icons/ExpandLess";
+import ExpandMore from "@material-ui/icons/ExpandMore";
 
 const useStyles = makeStyles((theme) => ({
   root: {
     height: "100%",
+    display: "flex",
+    flexDirection: "column",
+    justifyContent: "center",
+    alignItems: "center",
     marginLeft: theme.spacing(7),
     [theme.breakpoints.down("xs")]: {
       margin: 0,
@@ -41,17 +53,27 @@ const useStyles = makeStyles((theme) => ({
   },
   headerContainer: {
     display: "flex",
-    justifyContent: "center",
+    justifyContent: "space-around",
     alignItems: "center",
     padding: "1em 0",
     boxShadow:
       "0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19)",
+    [theme.breakpoints.down("xs")]: {
+      flexDirection: "column",
+      justifyContent: "center",
+    },
+  },
+  headerButtons: {
+    [theme.breakpoints.down("xs")]: {
+      width: "100%",
+    },
   },
   mapStyles: {
     height: "50%",
     width: "50%",
   },
   container: {
+    marginLeft: "30%",
     display: "flex",
     flexWrap: "wrap",
   },
@@ -59,12 +81,23 @@ const useStyles = makeStyles((theme) => ({
     margin: theme.spacing(1),
     minWidth: 120,
   },
+  dialogTitleStyle: {
+    display: "flex",
+    justifyContent: "space-between",
+    alignItems: "center",
+  },
   mapContainer: {
     position: "relative",
     height: "100%",
     display: "flex",
     justifyContent: "center",
     alignItems: "center",
+  },
+  filterNoFilter: {
+    background: theme.palette.primary.main,
+  },
+  nested: {
+    paddingLeft: theme.spacing(4),
   },
 }));
 
@@ -78,8 +111,22 @@ const MapPage = ({ userId, userName }) => {
   const [privacy, setPrivacy] = useState("");
   const [otherUserData, setOtherUserData] = useState(null);
   const [windowOpen, setWindowOpen] = useState(false);
+  const [filterHabitOpen, setFilterHabitOpen] = useState(false);
   const [listOfFilters, setListOfFilters] = useState(["No Filter", "Friends"]);
   const [filter, setFilter] = useState();
+  const options = {
+    strokeColor: "#FF0000",
+    strokeOpacity: 0.8,
+    strokeWeight: 2,
+    fillColor: "#FF0000",
+    fillOpacity: 0.35,
+    clickable: false,
+    draggable: false,
+    editable: false,
+    visible: true,
+    radius: 32,
+    zIndex: 1,
+  };
 
   useEffect(() => {
     getAllHabits();
@@ -130,6 +177,10 @@ const MapPage = ({ userId, userName }) => {
 
   const handleChange = (event) => {
     setPrivacy(event.target.value);
+  };
+
+  const handleFilterHabitOpen = () => {
+    setFilterHabitOpen(!filterHabitOpen);
   };
 
   const getLocation = () => {
@@ -206,6 +257,7 @@ const MapPage = ({ userId, userName }) => {
   };
 
   const getOtherLocations = (filterChoice) => {
+    setFilterHabitOpen(false);
     setFilterOpen(false);
     const requestOptions = {
       method: "POST",
@@ -257,14 +309,12 @@ const MapPage = ({ userId, userName }) => {
   };
 
   const mapStyles = {
-    height: "70%",
-    width: "70%",
-  };
-
-  const dialogTitleStyle = {
-    display: "flex",
-    justifyContent: "space-between",
-    alignItems: "center",
+    width: "75vw",
+    height: "75vh",
+    [theme.breakpoints.down("xs")]: {
+      width: "100px",
+      height: "100vh",
+    },
   };
 
   const handleWindowChange = (user) => {
@@ -272,173 +322,240 @@ const MapPage = ({ userId, userName }) => {
     setWindowOpen(!windowOpen);
   };
 
+  const handleFilterDialogClose = () => {
+    setFilterHabitOpen(false);
+    setFilterOpen(false);
+  };
+
   return (
     <div className={classes.root}>
-      <div className={classes.headerContainer}>
-        <div>
-          <Button onClick={handleClickOpen} color="primary">
-            Set Privacy
-          </Button>
-          <Dialog onClose={() => setFilterOpen(false)} open={filterOpen}>
-            <DialogTitle>
-              <Typography variant="h6">Select a Filter</Typography>
-              {filterOpen ? (
-                <IconButton onClick={() => setFilterOpen(false)}>
-                  <CloseIcon />
-                </IconButton>
-              ) : null}
-            </DialogTitle>
-            <List>
-              {listOfFilters.map((filter) => (
-                <ListItem
-                  button
-                  onClick={() => getOtherLocations(filter)}
-                  key={filter}
-                >
-                  <ListItemText primary={filter} />
-                </ListItem>
-              ))}
-            </List>
-          </Dialog>
-          <Dialog
-            disableBackdropClick
-            disableEscapeKeyDown
-            open={open}
-            onClose={handleClose}
-          >
-            <DialogTitle>Select one of the privacy options.</DialogTitle>
-            <DialogContent>
-              <Typography variant="subtitle1">
-                Private: Your location will not be visible to other users.
-              </Typography>
-              <Typography variant="subtitle1">
-                Friends: Your location will be visible only to your friends.
-              </Typography>
-              <Typography variant="subtitle1">
-                Public: Your location will be visible to other users.
-              </Typography>
-              <form className={classes.container}>
-                <FormControl className={classes.formControl}>
-                  <InputLabel id="demo-dialog-select-label">Privacy</InputLabel>
-                  <Select
-                    labelId="demo-dialog-select-label"
-                    id="demo-dialog-select"
-                    value={privacy}
-                    onChange={handleChange}
-                    input={<Input />}
-                  >
-                    <MenuItem value={"private"}>Private</MenuItem>
-                    <MenuItem value={"friends"}>Friends</MenuItem>
-                    <MenuItem value={"public"}>Public</MenuItem>
-                  </Select>
-                </FormControl>
-              </form>
-            </DialogContent>
-            <DialogActions>
-              <Button onClick={handleClose} color="primary">
-                Cancel
-              </Button>
-              <Button onClick={handleOkClose} color="primary">
-                Ok
-              </Button>
-            </DialogActions>
-          </Dialog>
-        </div>
-        <Button
-          variant="contained"
-          color="primary"
-          size="small"
-          onClick={getLocation}
-        >
-          Get Your Location
-        </Button>
-        <Button
-          variant="contained"
-          color="primary"
-          size="small"
-          endIcon={<FilterListIcon />}
-          onClick={() => setFilterOpen(true)}
-        >
-          Filter
-        </Button>
-      </div>
-      <LoadScript googleMapsApiKey="AIzaSyATAEeS2YuhKhzbSUNV2M2XH1H1YFztfZo">
-        <GoogleMap
-          zoom={7}
-          mapContainerStyle={{
-            width: "70vw",
-            height: "70vh",
-          }}
-          center={!location ? { lat: 53.46685, lng: -2.233884 } : location}
-        >
-          {!location ? null : <Marker position={location} label={userName} />}
-          {!otherUserData
-            ? null
-            : otherUserData.map((user) => {
-                return (
-                  <Marker
-                    key={user.user_id.user_name}
-                    position={JSON.parse(user.location)}
-                    onClick={() => handleWindowChange(user)}
-                    options={{
-                      icon: {
-                        url: user.profile_img,
-                        scaledSize: { width: 32, height: 32 },
-                      },
-                      shape: {
-                        coords: [null, null, 16],
-                        type: "circle",
-                      },
-                    }}
-                    label={user.user_id.user_name}
-                  />
-                );
-              })}
-          {windowOpen && selected ? (
-            <InfoWindow
-              position={JSON.parse(selected.location)}
-              onCloseClick={() => handleWindowChange(null)}
-            >
-              <div
-                style={{
-                  display: "flex",
-                  justifyContent: "space-evenly",
-                  alignItems: "center",
-                }}
+      <Grid container spacing={2}>
+        <Grid item xs={12} align="center">
+          <div className={classes.headerContainer}>
+            <div>
+              <Button
+                onClick={handleClickOpen}
+                color="primary"
+                startIcon={<FingerprintIcon />}
+                className={classes.headerButtons}
               >
-                <Tooltip title="View profile">
-                  <IconButton
-                    onClick={() =>
-                      history.push(`/profile/${selected.user_id.user_name}`)
-                    }
+                Set Privacy
+              </Button>
+
+              <Dialog onClose={handleFilterDialogClose} open={filterOpen}>
+                <MuiDialogTitle>
+                  <div
+                    style={{
+                      height: "100px",
+                      display: "flex",
+                      justifyContent: "space-between",
+                      alignItems: "center",
+                    }}
                   >
-                    <Avatar src={selected.profile_img} />
-                  </IconButton>
-                </Tooltip>
-                <div
+                    <Typography style={{ alignSelf: "flex-end" }} variant="h6">
+                      Filter the users shown on the map:
+                    </Typography>
+                    {filterOpen ? (
+                      <IconButton
+                        style={{ alignSelf: "flex-start" }}
+                        onClick={() => setFilterOpen(false)}
+                      >
+                        <CloseIcon />
+                      </IconButton>
+                    ) : null}
+                  </div>
+                </MuiDialogTitle>
+                <List>
+                  {console.log(listOfFilters)}
+                  <ListItem
+                    button
+                    alignItems="center"
+                    onClick={() => getOtherLocations(listOfFilters[0])}
+                    key={listOfFilters[0]}
+                  >
+                    <ListItemText primary="List all users" />
+                  </ListItem>
+                  <ListItem
+                    button
+                    alignItems="center"
+                    onClick={() => getOtherLocations(listOfFilters[1])}
+                    key={listOfFilters[1]}
+                  >
+                    <ListItemText primary={listOfFilters[1]} />
+                  </ListItem>
+                  <ListItem button onClick={handleFilterHabitOpen}>
+                    <ListItemText primary="Habits:" />
+                    {filterHabitOpen ? <ExpandLess /> : <ExpandMore />}
+                  </ListItem>
+                  <Collapse in={filterHabitOpen} timeout="auto" unmountOnExit>
+                    <List component="div" disablePadding>
+                      {listOfFilters.map((filter) => {
+                        return filter == "No Filter" ? null : filter ==
+                          "Friends" ? null : (
+                          <ListItem
+                            button
+                            alignItems="center"
+                            onClick={() => getOtherLocations(filter)}
+                            key={filter}
+                            className={classes.nested}
+                          >
+                            <ListItemText primary={filter} />
+                          </ListItem>
+                        );
+                      })}
+                    </List>
+                  </Collapse>
+                </List>
+              </Dialog>
+
+              <Dialog
+                disableBackdropClick
+                disableEscapeKeyDown
+                open={open}
+                onClose={handleClose}
+              >
+                <DialogTitle
+                  disableTypography
                   style={{
-                    display: "flex",
-                    flexDirection: "column",
-                    justifyContent: "space-between",
-                    alignItems: "flex-start",
+                    fontSize: "16px",
+                    fontWeight: "800",
+                    textTransform: "uppercase",
                   }}
                 >
-                  <Typography variant="h6">
-                    {selected.user_id.user_name}
+                  Select one of the privacy options:
+                </DialogTitle>
+                <DialogContent>
+                  <Typography style={{ fontSize: "12px" }} variant="body1">
+                    Private: Your location will not be visible to other users.
                   </Typography>
-                  <Typography
-                    variant="body1"
-                    component={Link}
-                    to={`/profile/${selected.user_id.user_name}`}
+                  <Typography style={{ fontSize: "12px" }} variant="body1">
+                    Friends: Your location will be visible only to your friends.
+                  </Typography>
+                  <Typography style={{ fontSize: "12px" }} variant="body1">
+                    Public: Your location will be visible to other users.
+                  </Typography>
+                  <form className={classes.container}>
+                    <FormControl className={classes.formControl}>
+                      <InputLabel id="demo-dialog-select-label">
+                        Privacy
+                      </InputLabel>
+                      <Select
+                        labelId="demo-dialog-select-label"
+                        id="demo-dialog-select"
+                        value={privacy}
+                        onChange={handleChange}
+                        input={<Input />}
+                      >
+                        <MenuItem value={"private"}>Private</MenuItem>
+                        <MenuItem value={"friends"}>Friends</MenuItem>
+                        <MenuItem value={"public"}>Public</MenuItem>
+                      </Select>
+                    </FormControl>
+                  </form>
+                </DialogContent>
+                <DialogActions>
+                  <Button onClick={handleClose} color="primary">
+                    Cancel
+                  </Button>
+                  <Button onClick={handleOkClose} color="primary">
+                    Ok
+                  </Button>
+                </DialogActions>
+              </Dialog>
+            </div>
+            <Button
+              variant="outlined"
+              color="secondary"
+              onClick={getLocation}
+              startIcon={<GpsFixedIcon />}
+              className={classes.headerButtons}
+            >
+              Get Your Location
+            </Button>
+            <Button
+              color="primary"
+              endIcon={<FilterListIcon />}
+              onClick={() => setFilterOpen(true)}
+              className={classes.headerButtons}
+            >
+              Filter
+            </Button>
+          </div>
+        </Grid>
+        <Grid item xs={12} align="center">
+          <LoadScript googleMapsApiKey="AIzaSyATAEeS2YuhKhzbSUNV2M2XH1H1YFztfZo">
+            <GoogleMap
+              zoom={15}
+              mapContainerStyle={mapStyles}
+              center={!location ? { lat: 53.46685, lng: -2.233884 } : location}
+            >
+              {!location ? null : (
+                <Marker position={location} label={userName} />
+              )}
+              <Circle
+                center={{ lat: 53.46685, lng: -2.233884 }}
+                // required
+                options={options}
+              />
+              {!otherUserData
+                ? null
+                : otherUserData.map((user) => {
+                    return (
+                      <Marker
+                        key={user.user_id.user_name}
+                        position={JSON.parse(user.location)}
+                        onClick={() => handleWindowChange(user)}
+                        label={user.user_id.user_name}
+                      />
+                    );
+                  })}
+              {windowOpen && selected ? (
+                <InfoWindow
+                  position={JSON.parse(selected.location)}
+                  onCloseClick={() => handleWindowChange(null)}
+                >
+                  <div
+                    style={{
+                      display: "flex",
+                      justifyContent: "space-evenly",
+                      alignItems: "center",
+                    }}
                   >
-                    Go to profile
-                  </Typography>
-                </div>
-              </div>
-            </InfoWindow>
-          ) : null}
-        </GoogleMap>
-      </LoadScript>
+                    <Tooltip title="View profile">
+                      <IconButton
+                        onClick={() =>
+                          history.push(`/profile/${selected.user_id.user_name}`)
+                        }
+                      >
+                        <Avatar src={selected.profile_img} />
+                      </IconButton>
+                    </Tooltip>
+                    <div
+                      style={{
+                        display: "flex",
+                        flexDirection: "column",
+                        justifyContent: "space-between",
+                        alignItems: "flex-start",
+                      }}
+                    >
+                      <Typography variant="h6">
+                        {selected.user_id.user_name}
+                      </Typography>
+                      <Typography
+                        variant="body1"
+                        component={Link}
+                        to={`/profile/${selected.user_id.user_name}`}
+                      >
+                        Go to profile
+                      </Typography>
+                    </div>
+                  </div>
+                </InfoWindow>
+              ) : null}
+            </GoogleMap>
+          </LoadScript>
+        </Grid>
+      </Grid>
     </div>
   );
 };
