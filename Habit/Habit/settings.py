9e7c27436c0af07c45dccd01d2af5e9c504ee5f7
Lonyin Chan <lonyin.chan@student.manchester.ac.uk>
2021-04-14 16:33:12 +0100

    Added media folder

--- Habit/Habit/settings.py
+++ Habit/Habit/settings.py
@@ -161,3 +161,5 @@ MEDIA_URL = '/'
 
 db_from_env = dj_database_url.config(conn_max_age=500)
 DATABASES['default'].update(db_from_env)
+
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
